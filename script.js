6074b014c242241945a6f57085aaa4ceb4bb5817
Smax456 <minnuaman.pilli@gmail.com>
2026-07-05 18:28:49 +0530

    Refactor start button click event to show message
    Updated the start button functionality to display a styled message.

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-document.getElementById("startBtn").onclick = function () {
-    alert("IT WORKS! 🎉");
+const startBtn = document.getElementById("startBtn");
+
+startBtn.onclick = function () {
+
+    document.body.innerHTML = `
+    <div style="
+        width:100vw;
+        height:100vh;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        background:#050816;
+        color:white;
+        font-size:40px;
+        font-family:Poppins,sans-serif;
+        text-align:center;
+    ">
+        Hello Geet ❤️<br>
+        Project STARDUST is working! 🚀
+    </div>
+    `;
+
 };
