c1a6d2f798b1c455b04b7e9306650c96306e3be4
Smax456 <minnuaman.pilli@gmail.com>
2026-07-05 20:28:51 +0530

    Refactor loading screen and birthday room logic

--- script.js
+++ script.js
@@ -2,78 +2,107 @@ const startBtn = document.getElementById("startBtn");
 
 startBtn.onclick = function () {
 
-const loadingScreen = document.getElementById("loadingScreen");  
+    const loadingScreen = document.getElementById("loadingScreen");
 
-loadingScreen.style.transition = "opacity 1.5s";  
-loadingScreen.style.opacity = "0";  
+    loadingScreen.style.transition = "opacity 1.5s";
+    loadingScreen.style.opacity = "0";
 
-setTimeout(() => {  
+    setTimeout(() => {
 
-    loadingScreen.innerHTML = `  
-    <div class="storyPage">  
+        loadingScreen.innerHTML = `
+        <div class="storyPage">
 
-        <h1>Hello Geet ❤️</h1>  
+            <h1>Hello Geet ❤️</h1>
 
-        <p id="story"></p>  
+            <p id="story"></p>
 
-        <button id="nextBtn" style="display:none;">  
-            Continue ✨  
-        </button>  
+            <button id="nextBtn" style="display:none;">
+                Continue ✨
+            </button>
 
-    </div>  
-    `;  
+        </div>
+        `;
 
-    loadingScreen.style.opacity = "1";  
+        loadingScreen.style.opacity = "1";
 
-    const message =  
-    "Some people become family not because of blood... but because of the place they earn in our hearts.";  
+        const message =
+        "Some people become family not because of blood... but because of the place they earn in our hearts.";
 
-    let i = 0;  
+        let i = 0;
 
-    const story = document.getElementById("story");  
+        const story = document.getElementById("story");
 
-    const interval = setInterval(() => {  
+        const interval = setInterval(() => {
 
-        story.innerHTML += message[i];  
+            story.innerHTML += message[i];
 
-        i++;  
+            i++;
 
-        if(i >= message.length){  
+            if(i >= message.length){
 
-            clearInterval(interval);  
+                clearInterval(interval);
 
-            document.getElementById("nextBtn").style.display = "inline-block";  
+                document.getElementById("nextBtn").style.display = "inline-block";
 
-        }  
+            }
 
-    },40);  
+        },40);
 
-    document.getElementById("nextBtn").onclick = birthdayRoom;  
+        document.getElementById("nextBtn").onclick = birthdayRoom;
 
-},1500);
+    },1500);
 
 }
 function birthdayRoom(){
 
 document.body.innerHTML = `
+<div class="birthdayRoom">
 
-<div class="birthdayRoom">  <div class="balloons">  
-    <div class="balloon b1"></div>  
-    <div class="balloon b2"></div>  
-    <div class="balloon b3"></div>  
-    <div class="balloon b4"></div>  
-    <div class="balloon b5"></div>  
-</div>  <h1>🎂 Happy Birthday Geet ❤️</h1>  <p>Your magical birthday journey has begun.</p>  <div class="cake">  <div class="plate"></div>  <div class="cake-base"></div>  <div class="cake-middle"></div>  <div class="cake-top"></div>  <div class="icing"></div>  <div class="drip drip1"></div>  
-<div class="drip drip2"></div>  
-<div class="drip drip3"></div>  <div class="candle">  
-<div class="flame"></div>  
-</div>  </div>  <button id="cakeBtn">  
-💨 Blow Candle  
-</button>  </div>  
-`;  document.getElementById("cakeBtn").onclick = function(){
+<div class="balloons">
+    <div class="balloon b1"></div>
+    <div class="balloon b2"></div>
+    <div class="balloon b3"></div>
+    <div class="balloon b4"></div>
+    <div class="balloon b5"></div>
+</div>
+
+<h1>🎂 Happy Birthday Geet ❤️</h1>
+
+<p>Your magical birthday journey has begun.</p>
+
+<div class="cake">
+
+<div class="plate"></div>
+
+<div class="cake-base"></div>
+
+<div class="cake-middle"></div>
+
+<div class="cake-top"></div>
+
+<div class="icing"></div>
+
+<div class="drip drip1"></div>
+<div class="drip drip2"></div>
+<div class="drip drip3"></div>
+
+<div class="candle">
+<div class="flame"></div>
+</div>
+
+</div>
+
+<button id="cakeBtn">
+💨 Blow Candle
+</button>
+
+</div>
+`;
+
+document.getElementById("cakeBtn").onclick = function(){
 
 alert("🎆 Fireworks Coming Soon!");
 
 }
 
-}
+                    }
